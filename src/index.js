78ff942936f0f391044a9b934534e9347a6840d0
Nagornyi <artnagornyi@gmail.com>
2023-02-22 19:46:41 +0300

    Деплой на GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMui from "./AppMui/AppMui";
+// import AppMui from "./AppMui/AppMui";
+import AppAnt from "./AppAnt/AppAnt";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <AppMui />
+    <AppAnt />
+    {/*<AppMui />*/}
   </>
 );
